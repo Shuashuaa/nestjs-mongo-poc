9045c8b38c117cbd08398f4f052b24c9f435f9c8
Shuashuaa <Shuashuaa@users.noreply.github.com>
2025-09-14 14:52:42 +0800

    added mongoose and user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app/app.controller';
+import { AppService } from './app/app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, MongooseModule.forRoot('mongodb://127.0.0.1/my_first_mongodb')],
   controllers: [AppController],
   providers: [AppService],
 })
